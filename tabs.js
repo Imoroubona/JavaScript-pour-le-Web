349af48aa12157e519a55bde458d04a863e71268
Bonaventure IMOROU <imoroubonaventure@gmail.com>
2020-02-27 12:00:55 +0100

    Mise en place du principe de base du système d'onglet

--- tabs.js
+++ tabs.js
@@ -1,32 +1,50 @@
 /**
- * Lorsqu'on clique sur un onglet, 
- *     
- *     
- *      Enfin on ajoute la classe active au contenu correspondant à l'onglet cliqué
- *  
+ * Lorsqu'on clique sur un onglet,
+ *    On retire la classe active de l'onglet actif
+ *    On ajoute la classe active à l'onglet cliqué
+ *    On retire la classe active du contenu actif
+ * On ajoute la classe active au contenu correspondant à l'onglet cliqué
+ * 
+    var tabs = document.querySelectorAll('.tabs a');
+    tabs.forEach(element => {
+
+       var div = element.parentNode.parentNode.parentNode;
+       var li = element.parentNode;
+
+      element.addEventListener('click', function(e){
+         if (li.classList.contains('active')) {
+            return false;
+         }
+         div.querySelector('.tabs .active').classList.remove('active');
+         li.className = 'active';
+         //On retire la classe active du contenu actif
+         div.querySelector('.tab-content.active').classList.remove('active');
+         //On ajoute la classe active au contenu correspondant à l'onglet cliqué
+         div.querySelector(this.getAttribute('href')).classList.add('active');
+      });
+    });
+ 
  */
 
- var tabs = document.querySelectorAll('.tabs a');
- tabs.forEach(element => {
-    element.addEventListener('click', function(e){
-       e.preventDefault();
+var tabs = document.querySelectorAll('.tabs a');
+ tabs.forEach((tab)=>{
+    tab.addEventListener('click', function(e){
        var div = this.parentNode.parentNode.parentNode;
        var li = this.parentNode;
-       
-       if(li.classList.contains('active')){
+       if (li.classList.contains('active')) {
           return false;
-       }
-       // On retire la classe active de l'onglet actif
-       div.querySelector('.tabs .active').classList.remove('active');
-     
+       } 
+      //On retire la classe active de l'onglet actif
+      div.querySelector('.tabs .active').classList.remove('active');
       //On ajoute la classe active à l'onglet cliqué
-       li.className = "active";
-
-      // On retire la classe active du contenu actif
+      li.className = "active";
+      //On retire la classe active du contenu actif
       div.querySelector('.tab-content.active').classList.remove('active');
+      //On ajoute la classe active au contenu correspondant à l'onglet cliqué
       div.querySelector(this.getAttribute('href')).classList.add('active');
 
-       
     });
+
  });
+
  

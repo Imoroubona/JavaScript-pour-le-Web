737097a373b1fa4e09d5989afa7890baf5ca2e43
Bonaventure IMOROU <imoroubonaventure@gmail.com>
2020-03-04 05:34:32 +0100

    Mise en place du système d'onglet sans animation

--- tabs.js
+++ tabs.js
@@ -26,25 +26,32 @@
  
  */
 
+var AfficherOnglet = function(a){
+   var div = a.parentNode.parentNode.parentNode;
+   var li = a.parentNode;
+   if (li.classList.contains('active')) {
+      return false;
+   } 
+  //On retire la classe active de l'onglet actif
+  div.querySelector('.tabs .active').classList.remove('active');
+  //On ajoute la classe active à l'onglet cliqué
+  li.className = "active";
+  //On retire la classe active du contenu actif
+  div.querySelector('.tab-content.active').classList.remove('active');
+  //On ajoute la classe active au contenu correspondant à l'onglet cliqué
+  div.querySelector(a.getAttribute('href')).classList.add('active');
+}
+
 var tabs = document.querySelectorAll('.tabs a');
  tabs.forEach((tab)=>{
     tab.addEventListener('click', function(e){
-       var div = this.parentNode.parentNode.parentNode;
-       var li = this.parentNode;
-       if (li.classList.contains('active')) {
-          return false;
-       } 
-      //On retire la classe active de l'onglet actif
-      div.querySelector('.tabs .active').classList.remove('active');
-      //On ajoute la classe active à l'onglet cliqué
-      li.className = "active";
-      //On retire la classe active du contenu actif
-      div.querySelector('.tab-content.active').classList.remove('active');
-      //On ajoute la classe active au contenu correspondant à l'onglet cliqué
-      div.querySelector(this.getAttribute('href')).classList.add('active');
-
+      AfficherOnglet(this);
     });
+});
 
- });
 
- 
+var hash = window.location.hash;
+var a = document.querySelector('a[href ="' + hash +' "]');
+if (a !== null && !a.parentNode.contains('active')) {
+   AfficherOnglet(a);
+}
